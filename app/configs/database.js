beaeeda88c02977ee0447d049b600e494c054c4c
NguyennChi <minhduy2205nguyen@gmail.com>
2022-10-27 17:28:03 +0700

    embed theme

---
+++ app/configs/database.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    username: 'nguyenchi1',
+    password: 'nguyenchi',
+    database: 'project_nodejs',
+    col_items: 'items',
+    col_sliders: 'sliders',
+    col_users: 'users',
+    col_categories: 'categories',
+    col_articles: 'articles',
+    col_menu: 'menu',
+    col_rss: 'rss',
+    col_settings: 'settings'
+};
---
+++ app/routes/backend/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const middleAuthentication = require(__path_middleware + 'auth');
+
+// router.use('/',middleAuthentication ,require('./dashboard'));
+router.use('/dashboard', require('./dashboard'));
+router.use('/items', require('./items'));
+router.use('/sliders', require('./sliders'));
+router.use('/users', require('./users'));
+router.use('/categories', require('./categories'));
+router.use('/articles', require('./articles'));
+router.use('/menu', require('./menu'));
+router.use('/rss', require('./rss'));
+router.use('/settings', require('./settings'));
+module.exports = router;
